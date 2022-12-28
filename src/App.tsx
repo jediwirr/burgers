e4e37f8870fe5c990c83f64a29fac6878646492f
anteon <a.pastuhov@getgain.ru>
2022-12-29 00:44:07 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  devServer: {
+    port: 3000,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],    
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts|js)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {
+                  'targets': 'defaults',
+                }],
+                '@babel/preset-react',
+              ],
+            },
+          },
+          'ts-loader',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader",
+        options: { limit: false },
+      },
+    ],
+  },
+};
